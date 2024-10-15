12e54231cecfa0aa77d52a2e5cb1730214bfeb73
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2024-10-14 19:22:28 -1000

    Learning 05 Class Inheritance

---
+++ prepare/Learning05/MathAssignment.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class MathAssignment : Assignment
+{
+	private string _textBookSection;
+	private string _problem;
+
+	public MathAssignment(string studentName, string topic, string textBookSection, string problem) : base(studentName, topic)
+	{
+		_textBookSection = textBookSection;
+		_problem = problem;
+
+	}
+
+	public string GetHomeworkList()
+	{
+		return $"Textbook Section: {_textBookSection} Problem: {_problem}";
+	}
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning05 World!");
-    }
+	static void Main(string[] args)
+	{
+		Assignment assignment = new Assignment("John Doe", "Mathematics");
+		Console.WriteLine(assignment.GetSummary());
+
+		MathAssignment mathAssignment = new MathAssignment("Jane Doe", "Statistics", "Chapter 3", "Problem 5");
+		Console.WriteLine(mathAssignment.GetSummary());
+		Console.WriteLine(mathAssignment.GetHomeworkList());
+
+		WritingAssignment writingAssignment = new WritingAssignment("John Smith", "History", "The American Revolution");
+		Console.WriteLine(writingAssignment.GetSummary());
+		Console.WriteLine(writingAssignment.GetWritingInformation());
+	}
 }
